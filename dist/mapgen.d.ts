eb2b18584995a204d48ad92d1c82233bb9f5f49f
Cody Ebberson <cody@ebberson.com>
2019-06-18 17:33:35 -0700

    Week 2 release

---
+++ dist/mapgen.d.ts
@@ -0,0 +1,15 @@
+import { Game, TileMap, RNG } from 'wglt';
+export declare const MAP_WIDTH = 64;
+export declare const MAP_HEIGHT = 64;
+export declare class MapGenerator {
+    readonly game: Game;
+    readonly map: TileMap;
+    readonly rng: RNG;
+    constructor(game: Game);
+    makeMap(): void;
+    private clearMap;
+    private createRoom;
+    private createHTunnel;
+    private createVTunnel;
+    private randomWall;
+}
